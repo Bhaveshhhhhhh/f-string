4aa1c348fb47c907cefcb0d59d363859cf4e753a
Bhavesh Choudhary <92804250+Bhaveshhhhhhh@users.noreply.github.com>
2022-02-02 17:35:25 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,17 @@
+#datatype of name is string
+name = "Bhavesh"
+#datatype of rollno is integer
+rollno = 17 
+# datatype of branch is string 
+branch = "IT"
+# datatype of pursuing is string 
+pursuing = "2nd Year"
+# datatype of first_pass is Boll
+first_pass = True
+# datatype of height is float
+height = 5.1
+# datatype of weight is integer
+weight = 50
+
+#f-String
+print(f"My name is {name}\nmy rollno is {rollno}\nI am studing in {branch} Branch\nI am in {pursuing}\nCompleted 1st year\nheight is{height}\nand weight is {weight}")
